aaf4673bda3b61c835431aa29db4879d3130482d
Tom Hulihan <hulihan.tom159@gmail.com>
2015-06-28 11:25:33 -0400

    Add Brainfuck.eval
    This method combines the entire pipeline together into one top-level
    function meant for external consumers.

--- lib/brainfuck.rb
+++ lib/brainfuck.rb
@@ -3,6 +3,25 @@ module Brainfuck
   # This is the base error for the gem from which the rest of the errors
   # subclass.
   BaseError = Class.new(StandardError)
+
+  module_function
+
+  # Evaluate an IO of commands
+  #
+  # @param commands [String, IO] the commands to execute.
+  # @param input [IO] the input from which the commands read.
+  # @param output [IO] the output to which the commands write.
+  # @param eof [Integer] the value to set when EOF is reached.
+  # @raise [BaseError] when there is a compiling or execution error.
+  def eval!(commands, input = $stdin, output = $stdout, eof = 0)
+    io = commands.is_a?(String) ? StringIO.new(commands) : commands
+    tokens = Parser.parse(io)
+    lexed = Lexer.run!(tokens.to_a)
+    commands = Unroll.unroll(lexed)
+    vm = VirtualMachine.new(input, output, eof)
+    vm.load!(commands)
+    vm.execute!
+  end
 end
 
 require 'brainfuck/version'
--- spec/lib/brainfuck_spec.rb
+++ spec/lib/brainfuck_spec.rb
@@ -4,4 +4,59 @@ describe Brainfuck do
   it 'has a version number' do
     expect(Brainfuck::VERSION).to_not be(nil)
   end
+
+  describe '.eval' do
+    let(:commands) { '' }
+    let(:input) { StringIO.new }
+    let(:output) { StringIO.new }
+    let(:eof) { 0 }
+
+    before { subject.eval!(commands, input, output, eof) }
+
+    context 'when the program is "Hello World"' do
+      let(:commands) do
+        <<-EOS
+          >++++++++[<+++++++++>-]<.>>+>+>++>[-]+<[>[->+<<++++>]<<]>.+++++++..+++
+          .>>+++++++.<<<[[-]<[-]>]<+++++++++++++++.>>.+++.------.--------.>>+.>+
+          +++.
+        EOS
+      end
+
+      it 'evaluates the commands' do
+        expect(output.string).to eq("Hello World!\n")
+      end
+    end
+
+    context 'when the program is "cat"' do
+      let(:commands) { ',[.,]' }
+      let(:string) { 'Howdy ho! Cowby hat!' }
+      let(:input) { StringIO.new(string) }
+
+      it 'evaluates the commands' do
+        expect(output.string).to eq(string)
+      end
+    end
+
+    context 'when the program is wc' do
+      let(:commands) do
+        <<-EOS
+          >>>+>>>>>+>>+>>+[<<],[
+              -[-[-[-[-[-[-[-[<+>-[>+<-[>-<-[-[-[<++[<++++++>-]<
+                  [>>[-<]<[>]<-]>>[<+>-[<->[-]]]]]]]]]]]]]]]]
+              <[-<<[-]+>]<<[>>>>>>+<<<<<<-]>[>]>>>>>>>+>[
+                  <+[
+                      >+++++++++<-[>-<-]++>[<+++++++>-[<->-]+[+>>>>>>]]
+                      <[>+<-]>[>>>>>++>[-]]+<
+                  ]>[-<<<<<<]>>>>
+              ],
+          ]+<++>>>[[+++++>>>>>>]<+>+[[<++++++++>-]<.<<<<<]>>>>>>>>]
+        EOS
+      end
+      let(:input) { StringIO.new("one two\nthree four\n") }
+
+      it 'evaluates the commands' do
+        expect(output.string).to eq("\t2\t4\t19\n")
+      end
+    end
+  end
 end
